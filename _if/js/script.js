9325e6c21d689ee70cf61c445d102685fff3550f
myungju <choi.mj66@gmail.com>
2016-11-15 09:26:42 +0900

    Add files via upload

---
+++ _if/js/script.js
@@ -0,0 +1,17 @@
+var my_var=78;
+
+if(my_var>=90){
+    console.log("A");
+}
+else if(my_var>=80){
+    console.log("B");
+}
+else if(my_var>=70){
+    console.log("C");
+}
+else if(my_var>=70){
+    console.log("D");
+}
+else{
+    console.log("E");
+}
