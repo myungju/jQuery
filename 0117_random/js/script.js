8235ec0f0b92576467fb4136aa17d515230e95ed
myungju <choi.mj66@gmail.com>
2016-11-18 17:11:12 +0900

    Add files via upload

--- 0117_random/js/script.js
+++ 0117_random/js/script.js
@@ -5,3 +5,8 @@ function random_fn(min,max){
 console.log(random_fn(10,30));
 console.log(random_fn(-10,20));
 console.log(random_fn(20,40));
+
+
+//Math.ceil : 올림처리
+//Math.round : 반올림처리
+//Math.floor : 내림처
