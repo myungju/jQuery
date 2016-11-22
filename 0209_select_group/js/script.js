14115e57ed12dd466ca92be4c9e63a0a2f88e12e
myungju <choi.mj66@gmail.com>
2016-11-22 19:10:47 +0900

    Add files via upload

---
+++ 0209_select_group/js/script.js
@@ -0,0 +1,5 @@
+window.onload=function(){
+    $("p,div,#nav").css({"list-style":"none", "background-color":"#666",color:"#fff"});
+}
+
+
---
+++ 0210_select_path/js/script.js
@@ -0,0 +1,8 @@
+window.onload=function(){
+    //$("div#container li").css({"list-style":"none","background-color":"#666",color:"#fff"});
+    //$("div>p").css({"list-style":"none","background-color":"#666",color:"#fff"});
+    $("#first+li").css({"list-style":"none","background-color":"#666",color:"#fff"});
+    //$("#first~li").css({"list-style":"none","background-color":"#666",color:"#fff"});
+}
+
+
