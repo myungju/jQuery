e132bb2ff78dec804671b7ce2c3fb28e8a89a589
myungju <choi.mj66@gmail.com>
2016-11-28 06:11:13 +0900

    Add files via upload

---
+++ 0213_select_filter/js/script.js
@@ -0,0 +1,8 @@
+window.onload=function(){
+    //$("li:contains('ap')").css({"background-color":"#666",color:"#fff"});
+    //$("ul:has(li)").css({"background-color":"#666",color:"#fff"});
+   //$("li:empty").text("dragon");
+    $("li:parent").css({"background-color":"#666",color:"#fff"});
+}
+
+
