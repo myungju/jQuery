b56b067b3471b24445ecd5e3b134fb0968454f40
myungju <choi.mj66@gmail.com>
2016-11-28 06:01:10 +0900

    Add files via upload

---
+++ 0212_select_filter/js/script.js
@@ -0,0 +1,12 @@
+window.onload=function(){
+    //$("li:eq(6)").css({"background-color":"#666",color:"#fff"});
+    //$("li:gt(5)").css({"background-color":"#666",color:"#fff"});
+    //$("li:lt(3)").css({"background-color":"#666",color:"#fff"});
+    //$("li:first, li:last").css({"background-color":"#666",color:"#fff"});
+    //$("li:even").css({"background-color":"#666",color:"#fff"});
+    //$("li:odd").css({"background-color":"#666",color:"#fff"});
+    $("li:not(:eq(7))").css({"background-color":"#666",color:"#fff"});
+    
+}
+
+
