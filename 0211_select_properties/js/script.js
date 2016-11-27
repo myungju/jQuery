0ff04b1f345f59b17c69400c1e8281ece6d75f5c
myungju <choi.mj66@gmail.com>
2016-11-28 05:38:40 +0900

    Add files via upload

---
+++ 0211_select_properties/js/script.js
@@ -0,0 +1,8 @@
+window.onload=function(){
+    //$("li a[href='#page2']").css({color:"#f90"});
+    //$("li a[href^='http']").css({color:"#f90"});
+    //$("li a[href$='com/']").css({color:"#f90"});
+    $("li a[href*='2']").css({color:"#f90"});
+}
+
+
