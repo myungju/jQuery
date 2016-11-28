8d3bf5cb1bb50c8d5b40ce00cd123255d30fc884
myungju <choi.mj66@gmail.com>
2016-11-28 14:48:57 +0900

    Add files via upload

---
+++ 0215_select_filter/js/script.js
@@ -0,0 +1,16 @@
+window.onload=function(){
+
+    //$("li:first").css({"background-color":"#666", color:"#fff"});
+    //$("li:first-child").css({"background-color":"#666", color:"#fff"});
+    //$("li:last").css({"background-color":"#666", color:"#fff"});
+    //$("li:last-child").css({"background-color":"#666", color:"#fff"});
+    //$("li:eq(2)").css({"background-color":"#666", color:"#fff"});
+    //$("li:nth-child(2)").css({"background-color":"#666", color:"#fff"});
+    //$("li:even").css({"background-color":"#666", color:"#fff"});
+    //$("li:nth-child(even)").css({"background-color":"#666", color:"#fff"});
+    //$("li:odd").css({"background-color":"#666", color:"#fff"});
+    //$("li:nth-child(odd)").css({"background-color":"#666", color:"#fff"});
+    $("li:nth-child(3n+2)").css({"background-color":"#666", color:"#fff"});
+}
+
+
